763c9aba52a4f3c2b73b5268ef0959077ed6fb98
Wum03 <willemschinck@gmail.com>
2023-10-24 23:45:00 +0200

    Created error message for a case in which game cannot be played correctly
    Also reset variable for rows to normal 3x3 configuration

--- Cheese House/Main.java
+++ Cheese House/Main.java
@@ -8,16 +8,21 @@ public class Main {
 
     public static int WIDTH = 640;
     public static int HEIGHT = 640;
-    public static int ROWS = 4;
+    public static int ROWS = 3;
     public static int WIN = 3;
     public static int SIZE = ROWS * ROWS;
 
-    //if rows < win then error message
+    //if rows < win then error message...?
     
 
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Normal Tic Tac Toe");
+
+        if (ROWS < WIN) {
+            System.out.println("win variable cannot be larger than rows variable");
+            return;
+        }
+        JFrame frame = new JFrame("Tic Tac Toe");
         TTTGame game = new TTTGame(new Color(121, 59, 49)); //121
         
     
